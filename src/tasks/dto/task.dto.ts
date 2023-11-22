2bffa36c62715f799b8ae4453200ca7e9ee697b8
Firelogic <nestor2_@live.cl>
2023-11-21 23:23:22 -0300

    Implementacion de Swagger

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,14 +1,18 @@
 import { TaskStatus } from "../task.entity"
 import {IsIn, IsNotEmpty, IsOptional, IsString, MinLength} from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTaskDTO{
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
+    @ApiProperty()
+
     title: string
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     description: string
 
 }
@@ -16,14 +20,20 @@ export class CreateTaskDTO{
 export class updateClassDTO{
     @IsString()
     @IsOptional()
+    @ApiProperty({
+        description: 'El titulo de la tarea',
+        required: false
+    })
     title?: string
 
     @IsString()
-    @IsOptional()
+    @IsOptional()  
+    @ApiProperty()
     description?: string
 
     @IsString()
     @IsOptional()
+    @ApiProperty()
     @IsIn([TaskStatus.PENDING, TaskStatus.DONE, TaskStatus.IN_PROGRESS])
     status?: TaskStatus
 }
